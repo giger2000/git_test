a27736a426ccbc3e53678f9a33c028b302912e18
giger2000 <alberto.anuncio@gmail.com>
2018-12-21 16:23:50 +0100

    entrega práctica SQL
    Entrega de les 7 consultes SQL de l'exercici

--- USFlights/exercicis.sql
+++ USFlights/exercicis.sql
@@ -1,8 +1,12 @@
-select * from Flights;
+select * 
+from Flights;
 /*1*/
-select count(*) from Flights;
+select count(*) 
+from Flights;
 /*2*/
-select AVG(ArrDelay) as Retard_promig_arribades,AVG(DepDelay)as Retard_promig_sortides, origin from flights group by origin;
+select AVG(ArrDelay) as Retard_promig_arribades,AVG(DepDelay)as Retard_promig_sortides, origin 
+from flights 
+group by origin;
 /*3*/
 select   Origin as origen,colYear as Anyo, colMonth as Mes, AVG(ArrTime) as promig_arribades 
 from flights 
@@ -15,3 +19,23 @@ left join USAirports a
 on f.Origin=a.IATA
 group by origin, colYear,colMonth 
 order by origin, colYear,colMonth;
+/*5*/
+select  c.description as Companyia, count(*) as cancelacions
+from flights f
+left join Carriers c
+on c.CarrierCode=f.UniqueCarrier
+where f.cancelled=1
+group by c.CarrierCode
+order by cancelacions desc;
+
+/*6*/
+select FlightNum, sum(distance) as DistanciaTotal
+from flights
+group by FlightNum
+order by DistanciaTotal desc
+limit 10;
+
+/*7*/
+select UniqueCarrier, AVG(ArrDelay) AS retard 
+from flights 
+group by UniqueCarrier having retard>10;
